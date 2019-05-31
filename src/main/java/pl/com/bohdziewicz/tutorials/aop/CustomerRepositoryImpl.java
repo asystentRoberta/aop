0e7a664d5277180d38be24fb77beef69979dddc1
Asystent Roberta <asystent.roberta@gmail.com>
2019-05-31 15:52:05 +0200

    Some new rules of code formating (it's mentioned in .idea/codeStyleConfig.xml). In my opinion it's clearly when in code is more empty lines... In context file I moved up bean customerRepository. Now all beans are declared in one place.

--- src/main/java/pl/com/bohdziewicz/tutorials/aop/CustomerRepositoryImpl.java
+++ src/main/java/pl/com/bohdziewicz/tutorials/aop/CustomerRepositoryImpl.java
@@ -1,15 +1,19 @@
 package pl.com.bohdziewicz.tutorials.aop;
 
 public class CustomerRepositoryImpl implements CustomerRepository {
+
     public Customer getCustomer(long customerId) {
+
         return null;
     }
 
     public String getCustomerName(long customerId) {
+
         return "Marcin";
     }
 
     public void addCustomer(Customer customer) {
+
         if (customer.getName() == null) {
             throw new RuntimeException("Customer name can't be null");
         } else {
