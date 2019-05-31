7f2f8e094e46ae9497dee66aa59d0bdca8388e51
Asystent Roberta <asystent.roberta@gmail.com>
2019-06-01 00:33:46 +0200

    Lesson 7. Adjusting... create Class BenchmarkAspect

---
+++ src/main/java/pl/com/bohdziewicz/tutorials/aop/BenchmarkAspect.java
@@ -0,0 +1,22 @@
+package pl.com.bohdziewicz.tutorials.aop;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class BenchmarkAspect {
+
+    String name;
+
+    private Logger logger = LogManager.getLogger(getClass().getName());
+
+    public void measureExecutionTime() {
+
+        long startTime = System.nanoTime();
+
+        //here method is called
+
+        long endTime = System.nanoTime();
+
+        logger.info("Executin time = " + (endTime - startTime) + " ns.");
+    }
+}
