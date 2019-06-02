7c6ece94906df35d3dff55c82a32091ff18d482c
Asystent Roberta <asystent.roberta@gmail.com>
2019-06-02 16:43:38 +0200

    Adjusting to lesson 9.

--- src/main/java/pl/com/bohdziewicz/tutorials/aop/SomeBean.java
+++ src/main/java/pl/com/bohdziewicz/tutorials/aop/SomeBean.java
@@ -2,20 +2,4 @@ package pl.com.bohdziewicz.tutorials.aop;
 
 public class SomeBean {
 
-    public void setAnotherThing(String anotherThing) {
-
-    }
-
-    public String getSomething() {
-
-        return null;
-    }
-
-    public void setSomething(String something) {
-
-    }
-
-    public void anotherMethod() {
-
-    }
 }
