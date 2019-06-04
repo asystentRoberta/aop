942abcf110e784df850ae792a2676b444e2e590a
Asystent Roberta <asystent.roberta@gmail.com>
2019-06-04 11:08:05 +0200

    Lesson 12. First aspects. Before, after, returning and throwing(not implementing yet).

--- src/main/java/pl/com/bohdziewicz/tutorials/aop/LoggingAspectWithAnnotations.java
+++ src/main/java/pl/com/bohdziewicz/tutorials/aop/LoggingAspectWithAnnotations.java
@@ -3,28 +3,46 @@ package pl.com.bohdziewicz.tutorials.aop;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
+@Aspect
+@Component
 public class LoggingAspectWithAnnotations {
 
     private Logger logger = LogManager.getLogger();
 
+    @Pointcut("execution(* *(..))")
+    public void allMethods() {
+
+    }
+
+    @Before("allMethods()")
     public void logBefore(JoinPoint joinPoint) {
 
-        logger.info("Executing method " + joinPoint.getSignature().getName());
+        logger.info("Aspect -> now I'm executing method: -> " + joinPoint.getSignature().getName());
     }
 
+    @After("execution(* *(..))")
     private void logAfter(JoinPoint joinPoint) {
 
-        logger.info("After method: " + joinPoint.getSignature().getName());
+        logger.info("Aspect -> after method: -> " + joinPoint.getSignature().getName());
     }
 
+    @AfterReturning(pointcut = "allMethods()", returning = "returnedValue")
     private void afterReturning(Object returnedValue) {
 
-        logger.info("Method rerurned " + returnedValue);
+        logger.info("Aspect -> method returned: -> " + returnedValue);
     }
 
+    @AfterThrowing(pointcut = "allMethods()", throwing = "exception")
     private void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
 
-        logger.info("Method " + joinPoint.getSignature().getName() + " has thrown " + exception);
+        logger.info("Aspect -> method -> " + joinPoint.getSignature().getName() + " -> has thrown -> " + exception);
     }
 }
