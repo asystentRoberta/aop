95bace20b2a7f06edc48045b44c41392fa0b5d96
Asystent Roberta <asystent.roberta@gmail.com>
2019-06-04 10:42:11 +0200

    Adjust fo lesson 12 - new approach to AOP (annotations). Project will be create as new one.

--- src/main/java/pl/com/bohdziewicz/tutorials/aop/BusinessMethod.java
+++
@@ -1,9 +0,0 @@
-package pl.com.bohdziewicz.tutorials.aop;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface BusinessMethod {
-
-}
--- src/main/java/pl/com/bohdziewicz/tutorials/aop/MainAop.java
+++ src/main/java/pl/com/bohdziewicz/tutorials/aop/MainAop.java
@@ -1,16 +1,8 @@
 package pl.com.bohdziewicz.tutorials.aop;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class MainAop {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        SomeBean someBean = context.getBean("someBean", SomeBean.class);
-
-        someBean.businessMethod();
-        someBean.someMethod("abc");
     }
 }
